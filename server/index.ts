12f15043b1448d487eeb2ece54873bb00ffd8104
Tyler Gobka Bright <tgbright88@gmail.com>
2025-03-06 16:40:52 +0000

    developing...

--- server/index.ts
+++ server/index.ts
@@ -5,8 +5,7 @@ import dotenv from 'dotenv';
 
 //Authentication Routes imports
 import jwt from 'jsonwebtoken';
-import { User } from './models/User';
-
+import { User } from '../models/User';
 
 dotenv.config();
 
@@ -16,9 +15,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI!)
+mongoose
+  .connect(process.env.MONGO_URI || 'mongodb://localhost:27017/auth-app',
+    { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch((err) => console.log('MongoDB connection error:', err));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
@@ -26,26 +27,30 @@ app.listen(PORT, () => {
 
 //Authentication Routes for SIGNUP
 app.post('/api/signup', async (req, res) => {
-    const {name, email, password } = req.body;
-    try {
-      const user = new User({name, email, password });
-      await user.save();
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
-      res.status(201).json({ token });
-    } catch (err) {
-      res.status(400).json({ error: 'User already exists' });
-    }
-  });
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
+      expiresIn: '1h',
+    });
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(400).json({ error: 'User already exists' });
+  }
+});
 
 //Authentication Routes for SIGNIN
 app.post('/api/signin', async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ error: 'Invalid credentials' });
-  
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ error: 'Invalid credentials' });
-  
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
-    res.json({ token });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ error: 'Invalid credentials' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ error: 'Invalid credentials' });
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
+    expiresIn: '1h',
   });
+  res.json({ token });
+});
